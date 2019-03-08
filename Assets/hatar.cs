b6b70bd307ce22d9ce4c7dafe085563e126fc4d8
gyorgyi-petra <44090425+gyorgyi-petra@users.noreply.github.com>
2019-03-08 18:50:25 +0100

    unity jatek

---
+++ Assets/hatar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hatar : MonoBehaviour {
+
+    public Transform player;
+    public float maxdistance;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) >= maxdistance)
+        {
+            player.position = transform.position;
+            player.GetComponent<playercontroller>().nullspeed();
+        }
+    }
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    public float BunnySpeed = 0;
+    public float BunnyRotate;
+    public Transform Bunny;
+    private float f = 0.0f;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        Bunny.Translate(Vector3.up * BunnySpeed * Time.deltaTime * 2f);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Bunny.transform.Rotate(Vector3.back * BunnyRotate * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Bunny.transform.Rotate(Vector3.forward * BunnyRotate * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (BunnySpeed >= 0)
+            {
+                if (BunnySpeed < 20)
+                {
+                    BunnySpeed++;
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (BunnySpeed >= 0)
+            {
+                if (BunnySpeed <= 20 && BunnySpeed != 0)
+                {
+                    BunnySpeed--;
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            BunnySpeed = 0;
+        }
+    }
+    public void nullspeed()
+    {
+        BunnySpeed = 0;
+    }
+}
